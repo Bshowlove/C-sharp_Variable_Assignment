e9595e9b956f6786cf68bc376a555159b7a255dd
HP 1030 G2 <HP 1030 G2@DESKTOP-6D8DSB4>
2022-02-23 17:57:31 +0800

    mr akinode assignment

---
+++ C-sharp_Variable_Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 4;
+        double d = 4.0;
+        string s = "Polyilaro ";
+
+
+        Console.WriteLine("Enter an integer value");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter a double value");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter a string value");
+        string c = Console.ReadLine();
+
+
+        Console.WriteLine(i + a);
+        Console.WriteLine(d + b);
+        Console.WriteLine(s + c);
+
+    }
+}
